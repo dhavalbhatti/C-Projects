4cbc1cc23396e999380593d28586610fdb9bbba3
dhavalbhatti <engineer.dhavalbhatti@gmail.com>
2023-08-28 21:05:01 +0530

    new

---
+++ string_uppercase.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str2[20];
+    printf("Please enter your name: ");
+    scanf("%s", &str2);
+    printf("%s \n", strupr(str2));
+    printf("%s \n", strlwr(str2));
+    return 0;
+    srt
+}
